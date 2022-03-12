53265ba204abd14282d1467f94a1b1ed75c77b46
DiegoNietoC <diego.n.x.000@gmail.com>
2022-03-12 14:32:20 -0600

    Solved problem with crud prototype

--- app.js
+++ app.js
@@ -1,39 +1,91 @@
-/**
- * Se necesita un dashboard con un login muy sencillo para la validación de un CRUD de los datos almacenados en el formulario de contacto.
- * En casos de éxito se maqueta con la siguiente API en listando la imagen, autor y la url de la descarga y es un slider:
- * 
- * Esta es la página
- * https://picsum.photos/
- * 
- * y esta la api a utlizar:
- * https://picsum.photos/v2/list
- * 
- */
-
 const API_URL = "https://picsum.photos/v2/list";
 const xhr = new XMLHttpRequest();
+
+let isContent = /\w+|[^\s]+/;
 
 function onRequestHandler(){
     if(this.readyState === 4 && this.status === 200){
         const data = JSON.parse(this.response);
         console.log(data);
 
-        const template = data.map( (x, index) => `
+        function show(){
+            const template = data.map( (x, index) => `
+            
             <div class="elem" id="test">
                 <img src="${x.download_url}">
                     <article>
-                    <h1>${x.author}</h1>
-                    <p>Link de descarga:</p>
-                    <a href="${x.url}">${x.url}</a>
+                        <h1>${x.author}</h1><container id="indiceContainer">ID. ${index}</container>
+                        <p>Link de descarga:</p>
+                        <a href="${x.url}">${x.url}</a>
                     </article>
             </div>
             `);
-        const apiAuthor = data.map( (b) => `<li>${b.author}</li>` );
-        const apiUrl = data.map( (c) => `<li>${c.url}</li>` );
+            document.getElementById('app').innerHTML = `<span id="">${template}<span>`;
+        }
+        show();
 
-        document.getElementById('app').innerHTML = `
-            <span id="">${template}<span>
-            `;
+        //Functionality:
+
+        //Delete:
+        document.getElementById('del').onclick = function(){
+            let idToDelete = document.getElementById('delId').value;
+            idToDelete = parseInt(idToDelete);
+
+            if(isNaN(idToDelete)){
+                alert("Please, write a number");
+            } else {
+                data.splice(idToDelete,1);
+                show();
+                alert("Deleted element "+idToDelete);  
+            }
+
+        };
+
+        //form:
+        document.getElementById('showForm').onclick = function(){
+            document.getElementById('addOrEdit').style.display="flex";
+        }
+        document.getElementById('closeForm').onclick = function(){
+            document.getElementById('addOrEdit').style.display="none";
+        }
+
+        //Create:
+        document.getElementById('add').onclick = function(){
+
+            let imgAdd = document.getElementById('Img').value;
+            let authAdd = document.getElementById('Auth').value;
+            let urlAdd = document.getElementById('Url').value;
+
+            if(!isContent.test(imgAdd) || !isContent.test(authAdd) || !isContent.test(urlAdd)){
+                alert("All fields are required");
+            } else {
+                data.push({"download_url":imgAdd, "author":authAdd, "url":urlAdd});
+                show();
+                alert("Created element.");  
+            }
+
+        };
+
+        //Edit:
+        document.getElementById('edit').onclick = function(){
+
+            let idEdit = document.getElementById('idAddOrEdit').value;
+            idEdit = parseInt(idEdit);
+            let imgAdd = document.getElementById('Img').value;
+            let authAdd = document.getElementById('Auth').value;
+            let urlAdd = document.getElementById('Url').value;
+
+            if(!isContent.test(imgAdd) || !isContent.test(authAdd) || !isContent.test(urlAdd) || isNaN(idEdit)){
+                alert("All fields are required");
+            } else {
+                alert(data[idEdit]);
+                data[idEdit] = {"download_url":imgAdd, "author":authAdd, "url":urlAdd};
+                show();
+                alert("Modified element.");  
+            }
+
+        };
+
     }
 }
 
@@ -41,26 +93,15 @@ xhr.addEventListener("load",onRequestHandler);
 xhr.open("GET", `${API_URL}`);
 xhr.send();
 
+
+//Buttons:
 const buttonRight = document.getElementById('right');
 const buttonLeft = document.getElementById('left');
 
 buttonRight.onclick = function () {
-  document.getElementById('app').scrollLeft += 100;
+  document.getElementById('app').scrollLeft += 150;
 };
 buttonLeft.onclick = function () {
-  document.getElementById('app').scrollLeft -= 100;
+  document.getElementById('app').scrollLeft -= 150;
 };
 
-document.getElementById('ok').onclick = function(){
-
-    let user = document.getElementById('nameAccess').value;
-    let mail = document.getElementById('email').value;
-    let pw = document.getElementById('pw').value;
-
-    if(pw=="a1b23e4c" && mail=="apiaccess@gmail.com" && user=="admin"){
-        window.location.href = "./slider.html";
-    } else {
-        alert("Nombre, email o contraseña incorrecta!");
-    }
-
-};
